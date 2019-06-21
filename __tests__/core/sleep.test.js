10f324eeda594137f2274be7d7effadaf225402f
nerjs <karifan.stap@gmail.com>
2019-06-21 06:12:20 +0300

    core/sleep

---
+++ __tests__/core/sleep.test.js
@@ -0,0 +1,24 @@
+const sleep = require('../../core/sleep')
+const allowDiff = require('../../math/allow_diff')
+
+test('sleep (es6)', async () => {
+    const t = 200,
+        d = Date.now();
+
+    await sleep(t)
+
+    const d2 = Date.now() - d
+    expect(allowDiff(d2, t, 20)).toEqual(true)
+})
+
+test('sleep (es5)', () => {
+    const t = 200,
+        d = Date.now();
+
+    sleep(t).then(() => {
+        const d2 = Date.now() - d
+        expect(allowDiff(d2, t, 20)).toEqual(true)        
+    })
+
+
+})
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+
+
+
+module.exports = {
+    projects: [
+        {
+            displayName: 'core utils',
+            testEnvironment: 'node',
+            testMatch: ['**/__tests__/core/*.(spec|test).js']
+        }
+      ]
+}
